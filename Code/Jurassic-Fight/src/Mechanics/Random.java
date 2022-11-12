46cbac83fcf3157238442c2697648f5cadb333bc
Loc5 <93589953+dest50@users.noreply.github.com>
2022-11-12 17:59:14 +0100

    Update Random.java

--- Code/Jurassic-Fight/src/Mechanics/Random.java
+++ Code/Jurassic-Fight/src/Mechanics/Random.java
@@ -2,7 +2,7 @@ package Mechanics;
 import java.util.Random ;
 
 public class Random {
-    public  int randomMove() # random number from 0 to 7 to deterine the destination
+    public  int randomMove() # random number from 0 to 7 to determine the destination
     {    
         Random random = new Random();
         int i = random.nextInt(7); 
